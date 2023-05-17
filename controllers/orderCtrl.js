042e0e0cd7471c54c1c6e28f1adc4a84d9c9b53a
Dariusz Franczak <darius32@op.pl>
2023-05-17 22:39:26 +0100

    work in progress on placing order

--- controllers/orderCtrl.js
+++ controllers/orderCtrl.js
@@ -1,6 +1,7 @@
 import expressAsyncHandler from 'express-async-handler';
 import Order from '../models/Order.js';
 import User from '../models/User.js';
+import Product from '../models/Product.js';
 
 // @desc    Create order
 // @route   POST /api/v1/orders
@@ -8,13 +9,33 @@ import User from '../models/User.js';
 export const createOrderCtrl = expressAsyncHandler(async (req, res) => {
 	// Get the payload (customer, orderItems, shippingAddress, totalPrice)
 	const { orderItems, shippingAddress, totalPrice } = req.body;
-	console.log('Payload is: ', { orderItems, shippingAddress, totalPrice });
 	// Find the User
 	const user = await User.findById(req.userAuthId);
-
 	// Check if order is not empty
+	if (orderItems?.length <= 0) {
+		throw new Error('No order items');
+	}
 	// Create order - save the order into DB
+	const order = await Order.create({
+		user: user?.id,
+		orderItems,
+		shippingAddress,
+		totalPrice,
+	});
+	// Push order int User
+	user.orders.push(order?._id);
+	await user.save();
 	// Update the product quantity
+	const products = await Product.find({ _id: { $in: orderItems } });
+	orderItems?.map(async (order) => {
+		const product = products?.find((product) => {
+			return product._id?.toString() === order._id?.toString();
+		});
+		if (product) {
+			product.totalSold += order.totalQtyBuying;
+		}
+		await product.save();
+	});
 	// Make a payment - stripe
 	// Implement payment webhook
 	// Update the user order
