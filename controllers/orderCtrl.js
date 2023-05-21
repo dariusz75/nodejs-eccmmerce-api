e6c4042e35348668d0d599cdf091b65d63012f75
Dariusz Franczak <darius32@op.pl>
2023-05-21 13:05:41 +0100

    stripe payment integrated

--- controllers/orderCtrl.js
+++ controllers/orderCtrl.js
@@ -1,52 +1,89 @@
-import expressAsyncHandler from 'express-async-handler';
+import asyncHandler from 'express-async-handler';
+import dotenv from 'dotenv';
+dotenv.config();
+import Stripe from 'stripe';
 import Order from '../models/Order.js';
 import User from '../models/User.js';
 import Product from '../models/Product.js';
 
+//stripe instance
+const stripe = new Stripe(process.env.STRIPE_KEY);
+
 // @desc    Create order
 // @route   POST /api/v1/orders
 // @access  Private
-export const createOrderCtrl = expressAsyncHandler(async (req, res) => {
-	// Get the payload (customer, orderItems, shippingAddress, totalPrice)
+export const createOrderCtrl = asyncHandler(async (req, res) => {
+	//Get the payload(customer, orderItems, shipppingAddress, totalPrice);
 	const { orderItems, shippingAddress, totalPrice } = req.body;
-	// Find the User
+	//Find the user
 	const user = await User.findById(req.userAuthId);
-	// Check if User has shipping address
+	//Check if user has shipping address
 	if (!user?.hasShippingAddress) {
 		throw new Error('Please provide shipping address');
 	}
-	// Check if order is not empty
+	//Check if order is not empty
 	if (orderItems?.length <= 0) {
-		throw new Error('No order items');
+		throw new Error('No Order Items');
 	}
-	// Create order - save the order into DB
+	//Place/create order - save into DB
 	const order = await Order.create({
-		user: user?.id,
+		user: user?._id,
 		orderItems,
 		shippingAddress,
+		// totalPrice: couponFound ? totalPrice - totalPrice * discount : totalPrice,
 		totalPrice,
 	});
-	// Push order int User
-	user.orders.push(order?._id);
-	await user.save();
-	// Update the product quantity
+
+	//Update the product qty
 	const products = await Product.find({ _id: { $in: orderItems } });
+
 	orderItems?.map(async (order) => {
 		const product = products?.find((product) => {
-			return product._id?.toString() === order._id?.toString();
+			return product?._id?.toString() === order?._id?.toString();
 		});
 		if (product) {
-			product.totalSold += order.totalQtyBuying;
+			product.totalSold += order.quantity;
+			console.log('orderRRRR is', order);
 		}
 		await product.save();
 	});
+	//push order into user
+	user.orders.push(order?._id);
+	await user.save();
+
 	// Make a payment - stripe
+	//convert order items to have same structure that stripe need
+	const convertedOrders = orderItems.map((item) => {
+		return {
+			price_data: {
+				currency: 'usd',
+				product_data: {
+					name: item?.name,
+					description: item?.description,
+				},
+				unit_amount: item?.price * 100,
+			},
+			quantity: item?.quantity,
+		};
+	});
+
+	const session = await stripe.checkout.sessions.create({
+		line_items: convertedOrders,
+		metadata: {
+			orderId: JSON.stringify(order?._id),
+		},
+		mode: 'payment',
+		success_url: 'http://localhost:3000/success',
+		cancel_url: 'http://localhost:3000/cancel',
+	});
+	res.send({ url: session.url });
+
 	// Implement payment webhook
 	// Update the user order
-	res.json({
-		success: true,
-		message: 'Order created',
-		order,
-		user,
-	});
+	// res.json({
+	// 	success: true,
+	// 	message: 'Order created',
+	// 	order,
+	// 	user,
+	// });
 });
--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,4 @@
-// Product schema
+//product schema
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
@@ -72,7 +72,7 @@ const ProductSchema = new Schema(
 	}
 );
 //Virtuals
-//Qty left
+//qty left
 ProductSchema.virtual('qtyLeft').get(function () {
 	const product = this;
 	return product.totalQty - product.totalSold;
@@ -82,36 +82,19 @@ ProductSchema.virtual('totalReviews').get(function () {
 	const product = this;
 	return product?.reviews?.length;
 });
-//Average Rating
+//average Rating
 ProductSchema.virtual('averageRating').get(function () {
 	let ratingsTotal = 0;
 	const product = this;
 	product?.reviews?.forEach((review) => {
 		ratingsTotal += review?.rating;
 	});
-	//Calc average rating
+	//calc average rating
 	const averageRating = Number(ratingsTotal / product?.reviews?.length).toFixed(
 		1
 	);
 	return averageRating;
 });
-// Virtuals
-ProductSchema.virtual('totalReviews').get(function () {
-	const product = this;
-	return product?.reviews?.length;
-});
-// Average rating
-ProductSchema.virtual('averageRating').get(function () {
-	let ratingsTotal = 0;
-	const product = this;
-	product?.reviews?.forEach((review) => {
-		ratingsTotal += review?.rating;
-	});
-
-	const averageRating = (ratingsTotal / product.reviews.length).toFixed(1);
-	return averageRating;
-});
-
 const Product = mongoose.model('Product', ProductSchema);
 
 export default Product;
